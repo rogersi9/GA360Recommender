9bf71d6b295587762b3a605cf93311537a6cec4f
Marc Eid <marc.mn.eid@gmail.com>
2024-03-09 11:59:50 -0500

    processed the data to add session duration for each visit for each visitor

--- src/main.py
+++ src/main.py
@@ -4,9 +4,15 @@ from pyspark.rdd import RDD
 from pyspark.sql import Row
 from pyspark.sql import DataFrame
 from pyspark.sql import SparkSession
+import pyspark.sql.functions as F
+from pyspark.sql.window import Window
+
+from pyspark.sql.functions import concat
 from pyspark.sql.functions import lit
-from pyspark.sql.functions import desc
-from pyspark.sql.functions import mean
+from pyspark.sql.functions import col
+from pyspark.sql.functions import lead
+from pyspark.sql.functions import when
+from pyspark.sql.functions import sum
 from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.ml.recommendation import ALS
 
@@ -50,12 +56,37 @@ Pyspark Dataframe
 
 '''
 
-def boiler_plate_function(filename):
+def calculate_session_duration(filename):
     spark = init_spark()
 
-    df_lines = spark.read.csv(filename, header=True, inferSchema=True)
+    df = spark.read.csv(filename, header=True, inferSchema=True)
 
-    df_lines.show(10)
+    # Create 'userId' by concatenating 'fullVisitorID' and 'visitNumber', separated by '-'
+    df = df.withColumn("userId", concat(col("fullVisitorID"), lit("-"), col("visitNumber")))
+
+    # Define window spec for ordering by time within each user session
+    windowSpec = Window.partitionBy("userId").orderBy("time")
+
+    # Calculate the difference in time for consecutive hits to get the pageview duration
+    df = df.withColumn("next_hit_time", lead("time", 1).over(windowSpec))
+    df = df.withColumn("pageview_duration", when(col("next_hit_time").isNull(), 0)
+                       .otherwise(col("next_hit_time") - col("time")))
+
+    # Filter for product detail views (where eCommerceAction.action_type == "2")
+    prod_view_df = df.filter(df['action_type'] == '2')
+
+    # Aggregate session durations by userId and productSKU, summing up durations to get total session duration
+    aggregate_web_stats = prod_view_df.groupBy("userId", "productSKU") \
+        .agg(sum("pageview_duration").alias("session_duration"))
+
+    # Optionally rename columns to match desired output
+    aggregate_web_stats = aggregate_web_stats.withColumnRenamed("productSKU", "itemId")
+
+    # Show the result
+    aggregate_web_stats.show(10)
+
+    aggregate_web_stats.write.csv('../data/data_v1.csv', header=True, mode='overwrite')
 
 
-boiler_plate_function("../data/raw data.csv")
+
+calculate_session_duration("../data/raw data.csv")
